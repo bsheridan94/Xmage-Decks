142b0361a50e86315372a42b6e7ebbc6aa39396d
Brian <controler12@gmail.com>
2015-10-05 21:46:05 -0400

    added new decks

---
+++ mage-client/startClient.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -Xms256m -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -jar ./lib/mage-client-1.4.3.jar &
---
+++ startMage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd mage-client
+bash startClient.sh
+cd ../mage-server
+bash startServer.sh
